ce5385f36e7f02de172b3dcd78cbac9219cf940f
prem <prem0814675274@gmail.com>
2026-09-03 00:13:18 +0700

    Create Shape.java

---
+++ Shape.java
@@ -0,0 +1,12 @@
+public abstract class Shape {
+    protected String name;
+    
+    public Shape(String name) {
+        this.name = name;
+    }
+    public abstract double getArea();
+    
+    public String getName() {
+        return name;
+    }
+}
